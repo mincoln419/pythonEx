6e080e29db69ca2170fb72e76fdfc29d62653033
mingeon cho <mincoln419@naver.com>
2023-10-16 10:01:13 +0900

    전체탐색

---
+++ 10.python_algorythm/01.search/01-03.search.py
@@ -0,0 +1,8 @@
+A,B,C,D,E,F = map(int, input().split())
+
+for x in range(-10_000, 10_000 + 1):
+    for y in range(-10_000, 10_000 + 1):
+        if A * x + B * y == C:
+            if D * x + E * y == F:
+                print("{} {}".format(x, y))
+                break

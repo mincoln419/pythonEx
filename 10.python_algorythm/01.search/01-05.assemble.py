e4633bc6177bfc27ab64249307037bfa1c46b92f
mingeon cho <mincoln419@naver.com>
2023-10-19 11:41:17 +0900

    checker ex 해결 -  반례찾기 필요

--- 10.python_algorythm/01.search/01-05.assemble.py
+++ 10.python_algorythm/01.search/01-05.assemble.py
@@ -1,21 +1,35 @@
+from operator import itemgetter, attrgetter
 N = int(input())
 
 members = [list(map(int, input().split())) for _ in range(N)]
 
-
-minX = 1_000_000
-minY = 1_000_000
-loc = [[0 for j in range(N)] for i in range(N)]
-for y in range(members.len()) :
-    for x in range(members.len()) : 
-        loc[y][x] = members[y][x]
+locX = []
+locY = []
+for i in range(N) :
+    memX = members[i][0]
+    memY = members[i][1]
+    if memX not in locX: 
+        locX.append(memX)
+    if memY not in locY:
+        locY.append(memY)
         
-print(loc)
+memberLen = []
 
-answer = []
-answer.sort()
+for i in range(N) :
+    memberLen.append([])
+
+for y in locY:
+    for x in locX:
+        lenAdd = 0
+        for i in range(len(members)):
+            lenAdd += abs(x - members[i][0]) + abs(y - members[i][1])
+            memberLen[i].append([x, y, lenAdd])     
 
 
-            
+for answer in memberLen :
+    answer = sorted(answer, key=itemgetter(2))
+    print(answer[0][2])
+
+
             
 

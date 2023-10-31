1dfc4f43166822fd1293c099a70e21aabf004242
mingeon cho <mincoln419@naver.com>
2023-10-31 15:33:53 +0900

    with no 주석

--- 10.python_algorythm/03.memory/tent_2304.py
+++ 10.python_algorythm/03.memory/tent_2304.py
@@ -11,25 +11,19 @@ for i in range(N):
 
 answer = topH
 
-# print(arr)
-
 if N == 2 :
     answer = abs(arr[1][0] - arr[0][0]) * min(arr[1][1], arr[1][0]) + max(arr[1][1], arr[1][0])
 
 if N > 2 :    
     curTop = arr[0][1]
     for i in range(1, topIndex + 1):
-        # print("arr[i][1]:{} , arr[i - 1][1]:{}".format(arr[i][1] , arr[i - 1][1]))
         answer += curTop * (arr[i][0] - arr[i - 1][0])        
-        # print("answer : {}".format(answer))
         if curTop < arr[i][1]:
             curTop = arr[i][1]
     
     curTop = arr[N - 1][1]
     for i in range(N - 2, topIndex - 1, -1):
-        # print("arr[i + 1][1]:{} - arr[i][1]:{}".format(arr[i + 1][0] ,arr[i][0]))
         answer += curTop * (arr[i + 1][0] - arr[i][0])        
-        # print(answer)
         if curTop < arr[i][1]:
             curTop = arr[i][1]
 

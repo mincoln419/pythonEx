eecf35474c61d3bb1cb5dc37765cd76b58f7fd1f
mingeon cho <mincoln419@naver.com>
2023-10-31 15:30:45 +0900

    tent with 주석

--- 10.python_algorythm/03.memory/tent_2304.py
+++ 10.python_algorythm/03.memory/tent_2304.py
@@ -1,8 +1,6 @@
 N = int(input())
 arr = [list(map(int, input().split())) for _ in range(N)]
 arr = sorted(arr, key=lambda lists: lists[0])
-print(*arr)
-
 
 topIndex = 0
 topH = 0
@@ -10,16 +8,29 @@ for i in range(N):
     if topH < arr[i][1]:
         topH = arr[i][1]
         topIndex = i
-    
-prefix = [0 for _ in range(N + 1)]
 
 answer = topH
-for i in range(topIndex):
-    prefix[i + 1] = max(prefix[i], arr[i][1])
+
+# print(arr)
+
+if N == 2 :
+    answer = abs(arr[1][0] - arr[0][0]) * min(arr[1][1], arr[1][0]) + max(arr[1][1], arr[1][0])
+
+if N > 2 :    
+    curTop = arr[0][1]
+    for i in range(1, topIndex + 1):
+        # print("arr[i][1]:{} , arr[i - 1][1]:{}".format(arr[i][1] , arr[i - 1][1]))
+        answer += curTop * (arr[i][0] - arr[i - 1][0])        
+        # print("answer : {}".format(answer))
+        if curTop < arr[i][1]:
+            curTop = arr[i][1]
     
-for i in range(N, topIndex, -1):
-    prefix[i - 1] = max(prefix[i] + arr[i][1], arr[i][1])
-    
-print(topIndex)
-print(topH)
-print(max(prefix))
+    curTop = arr[N - 1][1]
+    for i in range(N - 2, topIndex - 1, -1):
+        # print("arr[i + 1][1]:{} - arr[i][1]:{}".format(arr[i + 1][0] ,arr[i][0]))
+        answer += curTop * (arr[i + 1][0] - arr[i][0])        
+        # print(answer)
+        if curTop < arr[i][1]:
+            curTop = arr[i][1]
+
+print(answer)

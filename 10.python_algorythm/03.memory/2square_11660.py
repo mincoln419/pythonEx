35be9e878ce6a77cb579dd3f11b844fcbcd96ffc
mingeon cho <mincoln419@naver.com>
2023-11-08 15:07:43 +0900

    2square answer - import sys 를 해야 시간초과를 극복한다

--- 10.python_algorythm/03.memory/2square_11660.py
+++ 10.python_algorythm/03.memory/2square_11660.py
@@ -1,34 +1,18 @@
+import sys;
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
-table = [list(map(int, input().split())) for _ in range(N)]
-targetXy = [list(map(int, input().split())) for _ in range(M)] #x1, y1, x2, y2
-
-answerArr = [[0 for j in range(N)] for i in range(N)]
-
-answerArr[0][0] = table[0][0]
-
-
-for i in range(1 , N):       
-    answerArr[0][i] = answerArr[i - 1][0] + table[i][0]
-    answerArr[i][0] = answerArr[0][i - 1] + table[0][i]
-            
-
-for y in range(1, N) :
-    for x in range(1, N):        
-        answerArr[y][x] = answerArr[y - 1][x] + answerArr[y][x - 1] - answerArr[y - 1][x - 1] + table[y][x] 
-
-for x1, y1, x2, y2 in targetXy:
-    answer = 0
-    if x1 > 1 and y1 > 1 :
-        answer =  answerArr[y2-1][x2-1] - answerArr[y1 - 2][x2 - 1] - answerArr[y2 - 1][x1 - 2] + answerArr[y1 - 2][x1 - 2]
-        
-    if x1 == 1 and y1 > 1:
-        answer = answerArr[y2-1][x2-1] - answerArr[y1- 2][x2 - 1]
-        
-        
-    if x1 > 1 and y1 == 1:
-        answer = answerArr[y2-1][x2-1] - answerArr[y2- 1][x1 - 2]
-
-    if x1 ==1 and y1 == 1:
-        answer = answerArr[y2-1][x2-1]
-    print(answer)
+table = [[0] * (N + 1)]
+for _ in range(N):
+    table.append([0] + [int(x) for x in input().split()])
     
+answerArr = [[0] * (N + 1) for _ in range(N + 1)]
+for y in range(1 , N + 1) :
+    for x in range(1, N + 1):        
+        answerArr[y][x] = answerArr[y - 1][x] + answerArr[y][x - 1] - answerArr[y-1][x-1] + table[x][y] 
+
+for _ in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    ans = answerArr[y2][x2] - answerArr[y1 - 1][x2] - answerArr[y2][x1 - 1] + answerArr[y1 - 1][x1 - 1]
+    print(ans)
+

51f55278a33e394c819f4d272b67f5c24b17b7c4
mermer vivo <mincoln419@naver.com>
2023-11-11 13:50:55 +0900

    recursive baseball

---
+++ 10.python_algorythm/04.recursive/baseball_2503.py
@@ -0,0 +1,44 @@
+import sys;
+input = sys.stdin.readline
+
+N = int(input())
+hint = [list(map(int, input().split())) for _ in range(N)]
+
+answer = [0]
+
+def recur(i, target):
+    if target > 998:
+        return
+    if i == N :
+        if '0' not in str(target) and str(target)[0] != str(target)[1] and str(target)[1] != str(target)[2]:
+            answer[0] += 1
+        recur(0, target + 1)
+        return
+     
+    ball_point = 0
+    strike_point = 0
+    if str(hint[i][0])[0] in str(target) :
+        if str(hint[i][0])[0] == str(target)[0]:
+            strike_point +=1 
+        else : 
+            ball_point +=1
+    if str(hint[i][0])[1] in str(target) :
+        if str(hint[i][0])[1] == str(target)[1]:
+            strike_point +=1 
+        else : 
+            ball_point +=1
+    if str(hint[i][0])[2] in str(target) :
+        if str(hint[i][0])[2] == str(target)[2]:
+            strike_point +=1 
+        else : 
+            ball_point +=1
+    if strike_point == hint[i][1] and ball_point == hint[i][2]:
+        recur(i + 1, target)
+    else :
+        recur(0, target + 1)
+        
+recur(0, 123)
+print(*answer)
+    
+    
+

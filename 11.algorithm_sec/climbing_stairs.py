569dd6c2f5b2f7d95f8575d12a94d91187aff415
mingeon cho <mincoln419@naver.com>
2024-01-11 14:06:57 +0900

    clibming_stairs_top

--- 11.algorithm_sec/climbing_stairs.py
+++ 11.algorithm_sec/climbing_stairs.py
@@ -2,12 +2,14 @@ class Solution(object):
     def climbStairs(self, n):
 
         memo = {}
-        def dfs(n):
-            if n in {1, 2}:
-                memo[n] = n          
-            if n not in memo:
-                memo[n] = dfs(n - 1) + dfs(n - 2)
+        def bottom_up(n):
+            
+            for i in range(1, n + 1):
+                if i in {1, 2}:
+                    memo[i] = i
+                if i not in memo:
+                    memo[i] = memo[i - 1] + memo[i - 2]
             return memo[n]
-        return dfs(n)
+        return bottom_up(n)
 
         
